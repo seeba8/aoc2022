0f2bb5fad6e118d38b534c1d119e2df749b2d503
Sebastian Ober <sebastian.ober@posteo.de>
2022-12-10 19:53:08 +0100

    Solve day 9 part 2

--- src/day09.rs
+++ src/day09.rs
@@ -7,11 +7,24 @@ use crate::util::{Direction, read_input};
 pub fn solve() -> color_eyre::Result<()>{
     let input = read_input("day09.txt");
     println!("Day 09 part 1: {}", part1(&input)?);
+    println!("Day 09 part 1: {}", part2(&input)?);
     Ok(())
 }
 
 fn part1(input: &str) -> color_eyre::Result<usize> {
-    let mut area = Area::new();
+    let mut area = Area::new(2);
+    let instructions: Vec<Instruction> = input
+            .lines()
+            .map(str::parse)
+            .collect::<color_eyre::Result<Vec<_>>>()?;
+        for instruction in &instructions {
+            area.apply(instruction);
+        }
+    Ok(area.tail_visited.len())
+}
+
+fn part2(input: &str) -> color_eyre::Result<usize> {
+    let mut area = Area::new(10);
     let instructions: Vec<Instruction> = input
             .lines()
             .map(str::parse)
@@ -52,18 +65,16 @@ pub struct Point {
 
 #[derive(Debug)]
 pub struct Area {
-    head: Point,
-    tail: Point,
+    knots: Vec<Point>,
     tail_visited: HashSet<Point>,
 }
 
 impl Area {
-    pub fn new() -> Self {
+    pub fn new(knots: usize) -> Self {
         let mut visited = HashSet::new();
         visited.insert(Point::default());
         Self {
-            head: Point::default(),
-            tail: Point::default(),
+            knots: vec![Point::default(); knots],
             tail_visited: visited,
         }
     }
@@ -76,32 +87,35 @@ impl Area {
 
     fn move_head(&mut self, direction: Direction) {
         match direction {
-            Direction::Up => self.head.y += 1,
-            Direction::Right => self.head.x += 1,
-            Direction::Down => self.head.y -= 1,
-            Direction::Left => self.head.x -= 1,
+            Direction::Up => self.knots[0].y += 1,
+            Direction::Right => self.knots[0].x += 1,
+            Direction::Down => self.knots[0].y -= 1,
+            Direction::Left => self.knots[0].x -= 1,
         }
         self.follow_tail();
     }
 
     fn follow_tail(&mut self) {
-        if self.head.x.abs_diff(self.tail.x) <= 1 && self.head.y.abs_diff(self.tail.y) <= 1 {
-            // They are touching
-            return;
+        for i in 1..self.knots.len() {
+            if self.knots[i-1].x.abs_diff(self.knots[i].x) <= 1 && self.knots[i-1].y.abs_diff(self.knots[i].y) <= 1 {
+                // They are touching
+                return;
+            }
+    
+            if self.knots[i-1].x == self.knots[i].x {
+                // same column
+                self.knots[i].y += (self.knots[i-1].y - self.knots[i].y).signum();
+            } else if self.knots[i-1].y == self.knots[i].y {
+                // same row
+                self.knots[i].x += (self.knots[i-1].x - self.knots[i].x).signum();
+            } else {
+                // diagonal
+                self.knots[i].y += (self.knots[i-1].y - self.knots[i].y).signum();
+                self.knots[i].x += (self.knots[i-1].x - self.knots[i].x).signum();
+            }
         }
-
-        if self.head.x == self.tail.x {
-            // same column
-            self.tail.y += (self.head.y - self.tail.y).signum();
-        } else if self.head.y == self.tail.y {
-            // same row
-            self.tail.x += (self.head.x - self.tail.x).signum();
-        } else {
-            // diagonal
-            self.tail.y += (self.head.y - self.tail.y).signum();
-            self.tail.x += (self.head.x - self.tail.x).signum();
-        }
-        self.tail_visited.insert(self.tail);
+        
+        self.tail_visited.insert(*self.knots.last().expect("There must be at least 1 knot"));
     }
 }
 
@@ -111,11 +125,16 @@ pub mod tests {
 
     use super::*;
     #[test]
-    fn it_follows() -> color_eyre::Result<()> {
-        
+    fn it_follows_length2() -> color_eyre::Result<()> {
         let input = read_example("day09.txt");
-        
         assert_eq!(13, part1(&input)?);
+        Ok(())
+    }
+
+    #[test]
+    fn it_follows_length10() -> color_eyre::Result<()> {
+        assert_eq!(1, part2(&read_example("day09.txt"))?);
+        assert_eq!(36, part2(&read_example("day09_2.txt"))?);
         Ok(())
     }
 }
